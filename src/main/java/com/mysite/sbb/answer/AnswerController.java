62af82fbf4e2390cc99bc603e81d1d596f8c8444
haemin4738 <leeheamin12@gmail.com>
2025-08-20 10:48:24 +0900

    JSB 2-17

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,25 +2,31 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping("/answer")
 @RequiredArgsConstructor
 @Controller
 public class AnswerController {
-    private final QuestionService questionService;
     private final AnswerService answerService;
+    private final QuestionService questionService;
 
-    @PostMapping("/create/{id}")
-    public String createAnswer(@PathVariable("id") Integer id,
-                               @RequestParam(value = "content") String content) {
+    @PostMapping("/answer/create/{id}")
+    public String create(Model model, @PathVariable Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = questionService.getQuestion(id);
-        answerService.create(question, content);
-        return String.format("redirect:/question/detail/%d", id);
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+
+        answerService.create(question, answerForm.getContent());
+
+        return "redirect:/question/detail/%d".formatted(id);
     }
 }
